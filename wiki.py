b7d9078462085f6bd34be3b9312c25bb1e2b6bab
ka-arutyunov <78428096+ka-arutyunov@users.noreply.github.com>
2021-04-19 12:22:11 +0300

    update wiki script

--- wiki.py
+++ wiki.py
@@ -29,28 +29,7 @@ def words_count(words):
         print("{}: {}".format(k, animal_counts[k]))
 
 
-# def words_count2(words):
-#     filtered_words = []
-#     for word in words:
-#         animal = re.search(r'^[А-Я]\w.*', word.title)
-#         if animal:
-#             filtered_words.append(animal.group())
-#
-#     filtered_words[:-4].sort()
-#     cur_count = 0
-#     cur_letter = filtered_words[0][0]
-#     for i in filtered_words:
-#         if cur_letter == i[0]:
-#             cur_count += 1
-#         else:
-#             print("{}: {}".format(cur_letter, cur_count))
-#             cur_count = 0
-#             cur_letter = i[0][0]
-
-
 category = wiki.page("Категория:Животные_по_алфавиту")
 values = category.categorymembers.values()
 words_count(values)
 # words_count2(values)
-
-
